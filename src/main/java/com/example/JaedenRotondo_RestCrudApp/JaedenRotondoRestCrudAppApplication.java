c05dcb95c388270c01effdfb9833d2ee0900063e
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2026-03-04 13:43:48 -0500

    init with application.properties

---
+++ src/main/java/com/example/JaedenRotondo_RestCrudApp/JaedenRotondoRestCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.JaedenRotondo_RestCrudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaedenRotondoRestCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaedenRotondoRestCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JaedenRotondo_RestCrudApp/JaedenRotondoRestCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JaedenRotondo_RestCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaedenRotondoRestCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
